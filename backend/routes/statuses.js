b0c3cdc289bb5162e15fcb9768bca80246ad1682
Lokesh Jaiswar <lokeshjaiswar.dev@gmail.com>
2026-05-29 23:28:31 +0530

    push

--- backend/routes/statuses.js
+++ backend/routes/statuses.js
@@ -5,7 +5,11 @@ import { db } from '../database.js';
 
 const router = express.Router();
 
-router.get('/', authenticateToken, async (req, res) => {
+// router.get('/', authenticateToken, async (req, res) => {
+//   const statuses = await db.all('SELECT id, name FROM statuses ORDER BY id');
+//   res.json(statuses);
+// });
+router.get('/',  async (req, res) => {
   const statuses = await db.all('SELECT id, name FROM statuses ORDER BY id');
   res.json(statuses);
 });
